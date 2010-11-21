893bf6b3b185c632ca66ff73f8ecf1a19769c0f1
bod <bod@269df3cd-7b44-418d-811d-5be6b21a80ad>
2010-11-21 00:35:21 +0000

    qsdf
    git-svn-id: svn://jraf.org/repos/carmen/NounoursIOS/trunk@1866 269df3cd-7b44-418d-811d-5be6b21a80ad

---
+++ src/io/FlingAnimationReader.h
@@ -0,0 +1,17 @@
+//
+//  FlingAnimationReader.h
+//  Nounours
+//
+//  Created by Carmen Alvarez on 11/20/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "NounoursReader.h"
+
+@interface FlingAnimationReader : NounoursReader {
+@private NSMutableArray* flingAnimations;
+}
+-(FlingAnimationReader*) initFlingAnimationReader:(NSString*)pfilename;
+@property(retain,readonly) NSArray* flingAnimations;
+@end
