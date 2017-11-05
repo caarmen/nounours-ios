672d3634ba1982b999f414b87434e8551ee351cd
Carmen Alvarez <c@rmen.ca>
2017-11-05 02:52:03 +0100

    Added GPL license

--- Nounours/src/data/Animation.h
+++ Nounours/src/data/Animation.h
@@ -1,9 +1,20 @@
 //
-//  Animation.h
-//  Nounours
+// Copyright (c) 2010 - 2011 Carmen Alvarez
 //
-//  Created by Carmen Alvarez on 11/20/10.
-//  Copyright 2010-2011 Carmen Alvarez. All rights reserved.
+// This file is part of Nounours for iOS.
+//
+// Nounours for iOS is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Nounours for iOS is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Nounours for iOS.  If not, see <http://www.gnu.org/licenses/>.
 //
 
 #import <Foundation/Foundation.h>
