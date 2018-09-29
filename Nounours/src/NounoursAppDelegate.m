2ec561170a72fc13151d078f5f2cf8a5c78eafe7
Carmen Alvarez <c@rmen.ca>
2018-09-29 15:51:55 +0200

    Force portrait mode

--- Nounours/src/NounoursAppDelegate.m
+++ Nounours/src/NounoursAppDelegate.m
@@ -66,7 +66,9 @@
 	[viewController.nounours savePreferences];
 	
 }
-
+- (UIInterfaceOrientationMask)application:(UIApplication *)application supportedInterfaceOrientationsForWindow:(UIWindow *)window {
+	return UIInterfaceOrientationMaskPortrait;
+}
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
     /*
